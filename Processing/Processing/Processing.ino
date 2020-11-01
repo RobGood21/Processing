e89496a3a2137e13c585ba9a3276e925cb19d27d
Rob Antonisse <robantonisse@gmail.com>
2020-11-01 22:45:42 +0100

    Processing

--- Processing/Processing/Processing.ino
+++ Processing/Processing/Processing.ino
@@ -4,16 +4,46 @@
  Author:	Rob Antonisse
 
  Sketch voor een interface tussen arduino en Processing
-
-
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+//declaraties en libraries
+#include <SPI.h>
+#include <Wire.h>
 
+#include <Adafruit_GFX.h>
+#include <splash.h>
+#include <Adafruit_SSD1306.h>
+
+//constructors
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+
+byte received;
+
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("Hallo, even een berichtje van Art Duino.");
+
+	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+	display.clearDisplay();
+	display.setTextColor(WHITE);
+	display.setTextSize(2);
+	display.setCursor(10, 10);
+	display.print("hallo");
+	display.drawRect(1, 1, 120, 60,WHITE);
+	display.display();
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
-  
+	// reply only when you receive data:
+	//if (Serial.available() > 0) {
+		// read the incoming byte:
+		//received = Serial.read();
+
+		// say what you got:
+		//Serial.print("I received: ");
+		//Serial.print(char(received));
+	//}
 }
