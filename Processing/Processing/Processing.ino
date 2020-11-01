d2029f2785350f5c700346e8c2e18c66887ea3ad
Rob Antonisse <robantonisse@gmail.com>
2020-11-01 12:11:46 +0100

    Begin

---
+++ Processing/Processing/Processing.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		Processing.ino
+ Created:	11/1/2020 12:10:15 PM
+ Author:	Rob Antonisse
+
+ Sketch voor een interface tussen arduino en Processing
+
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
