0b5eb6a8de3134db258802220bd32bcb662fd7c4
Rob Antonisse <robantonisse@gmail.com>
2020-12-11 16:28:15 +0100

    bp

--- Processing/Sketch1/BomBPuzzel.ino
+++ Processing/Sketch1/BomBPuzzel.ino
@@ -12,13 +12,38 @@
 #include <EEPROM.h>
 #include <FastLed.h>
 
+//declarations
+#define fl FastLED.show();
+
+byte aantalpix = 24;
+CRGB pix[24];
+
+//temps
+unsigned long tijd;
+byte pixcount;
 
 
 void setup() {
+	FastLED.addLeds<NEOPIXEL, 8 >(pix, 24);
+	FastLED.setBrightness(50);
 
 }
 
 
 void loop() {
-  
+	if (millis() - tijd > 1000) {
+		tijd = millis();
+		FL_exe();
+  }
+}
+void FL_exe() {
+	pixcount ++;
+	FastLED.clearData();
+
+
+	if (pixcount > aantalpix-1) pixcount = 0;
+	pix[pixcount].r = random(0, 255);
+	pix[pixcount].g = random(0, 255);
+	pix[pixcount].b = random(0, 255);
+	fl;
 }
