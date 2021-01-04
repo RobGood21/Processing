a3e3dba598e05c62bb112cdae4e1acbb7101898d
Rob Antonisse <robantonisse@gmail.com>
2021-01-04 14:26:41 +0100

    BrideExitstart

---
+++ Processing/BrideExit/BrideExit.ino
@@ -0,0 +1,35 @@
+/*
+ Name:		BrideExit.ino
+ Created:	1/4/2021 2:16:15 PM
+ Author:	Rob Antonisse
+
+ Exit number puzzel. To entry the correct number wil release doorlock 
+
+
+
+
+*/
+
+
+#include <gfxfont.h>
+#include <Adafruit_SPITFT_Macros.h>
+#include <Adafruit_SPITFT.h>
+#include <Adafruit_GFX.h>
+#include <EEPROM.h>
+#include <splash.h>
+#include <Adafruit_SSD1306.h>
+
+//declaraties
+
+
+void setup() {
+	Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+
+
+}
+
+
+void loop() {
+  
+}
